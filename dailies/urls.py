ffa7097a2dd622fc596c3f63934d5f0d46b4061c
Sarah Elderderi <gongoleez@karems-air.home>
2025-10-12 13:11:52 +0100

    add pages for morning and evening

--- dailies/urls.py
+++ dailies/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'dailies'
 urlpatterns = [
     path('', views.home, name='dailies-home'),
+    path('morning/', views.dailiesMorning, name='morning'),
+    path('evening/', views.dailiesEvening, name='evening'),
 ]
--- dailies/views.py
+++ dailies/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'dailies/home.html')
+
+def dailiesMorning(request):
+    return render(request, 'dailies/dailies-morning.html')
+
+def dailiesEvening(request):
+    return render(request, 'dailies/dailies-evening.html')
