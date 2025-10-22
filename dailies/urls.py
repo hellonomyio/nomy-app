2ad8892b1811b02e1a849aee896eb8d381defec5
Sarah Elderderi <gongoleez@karems-air.home>
2025-10-22 15:37:52 +0100

    add submenu

--- dailies/urls.py
+++ dailies/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'dailies'
 urlpatterns = [
     path('', views.home, name='dailies-home'),
-    path('morning/', views.dailiesMorning, name='morning'),
+    path('morning-affirmation/', views.dailiesMorningAffirmation, name='morningAffirmation'),
+    path('morning-goal/', views.dailiesMorningGoal, name='morningGoal'),
     path('evening/', views.dailiesEvening, name='evening'),
 ]
