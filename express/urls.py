fc1fa369970a3f17c3c5e96a58cf064d666dbafe
hellonomyio <hello.nomyio@gmail.com>
2025-12-19 10:01:49 +0000

    remove expresscustom

--- express/urls.py
+++ express/urls.py
@@ -4,7 +4,6 @@ app_name = 'express'
 urlpatterns = [
     path('', views.home, name='express-home'),
     path('create/', views.createScenarioPage, name='create-scenario'),
-    path('custom/', views.expressCustom, name='express-custom'),
     path('speak/', views.expressSpeak, name='express-speak'),
     path('text/', views.expressText, name='express-text'),
     path('scenario/', views.expressScenario, name='express-scenario'),
