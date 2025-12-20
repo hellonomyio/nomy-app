481e1db8d5875f3c1684990d968e10e595b1034f
hellonomyio <hello.nomyio@gmail.com>
2025-12-20 12:02:42 +0000

    add super user

--- nomy/settings.py
+++ nomy/settings.py
@@ -2,7 +2,7 @@
 Django settings for nomy project.
 Updated for deployment on Render / Railway (auto-deploy from GitHub)
 """
-
+from django.contrib.auth import get_user_model
 import os
 from pathlib import Path
 import dj_database_url
@@ -174,3 +174,12 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 LOGIN_URL = '/login/'
+
+if os.environ.get("CREATE_SUPERUSER") == "True":
+    User = get_user_model()
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+    if username and password and not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, password=password, email=email)
