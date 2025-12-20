a01e7887d64c6a74354fc3b73e52e68f67504ae5
hellonomyio <hello.nomyio@gmail.com>
2025-12-20 11:56:36 +0000

    add psycopg2-binary

--- core/views.py
+++ core/views.py
@@ -5,17 +5,22 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, "core/home.html")
 
+@login_required
 def intro(request):
     return render(request, "core/intro.html")
 
+@login_required
 def options(request):
     return render(request, "core/options.html")
 
+@login_required
 def about(request):
     return render(request, "core/about.html")
 
+@login_required
 def settings(request):
     return render(request, "core/settings.html")
 
+@login_required
 def help(request):
     return render(request, "core/help.html")
